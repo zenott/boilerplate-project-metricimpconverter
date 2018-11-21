bd45b04eb99144f8aeecbf5e790a2c007af7d7c4
Glitch (watery-process) <none>
2018-11-21 13:10:55 +0000

    Fix methods

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -39,7 +39,7 @@ suite('Functional Tests', function() {
         .query({input: '32g'})
         .end(function(err, res){
           assert.equal(res.status, 200);
-          assert.equal(res.body.initUnit, 'invalid unit');
+          assert.equal(res.body, 'invalid unit');
           done();
         });
       });
@@ -50,7 +50,7 @@ suite('Functional Tests', function() {
         .query({input: '3/7.2/4kg'})
         .end(function(err, res){
           assert.equal(res.status, 200);
-          assert.equal(res.body.initNum, 'invalid number');;
+          assert.equal(res.body, 'invalid number');;
           done();
         });
       });  
@@ -60,11 +60,7 @@ suite('Functional Tests', function() {
         .get('/api/convert')
         .query({input: '3/7.2/4kilomegagram'})
         .end(function(err, res){
-          assert.equal(res.status, 200);
-          assert.equal(res.body.initNum, 10);
-          assert.equal(res.body.initUnit, 'L');
-          assert.approximately(res.body.returnNum, 2.64172, 0.1);
-          assert.equal(res.body.returnUnit, 'gal');
+          assert.equal(res.body, 'invalid number and unit');
           done();
         });
       });
